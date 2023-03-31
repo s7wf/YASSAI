9eb690a654b00abc540d076418b2cd31ac222b25
s7wf <82785944+s7wf@users.noreply.github.com>
2023-03-31 13:50:32 -0700

    Errors galore. Attempting to make a live visualization of the analysis.

--- main.py
+++ main.py
@@ -3,6 +3,66 @@ import pytesseract
 import pandas as pd
 import numpy as np
 
+def process_frame(frame):
+    # Define the expected size and position of the card in the image
+    card_x = 100  # Adjust this to match the position of the card in your images
+    card_y = 100  # Adjust this to match the position of the card in your images
+    card_w = 500  # Adjust this to match the size of the card in your images
+    card_h = 300  # Adjust this to match the size of the card in your images
+
+    # Apply a mask to the image to isolate the area where the card is located
+    mask = np.zeros_like(frame)
+    cv2.rectangle(mask, (card_x, card_y), (card_x + card_w, card_y + card_h), (255, 255, 255), -1)
+    masked_frame = cv2.bitwise_and(frame, mask)
+
+    # Use OCR to detect and extract the text in row 1
+    row1_roi = masked_frame[card_y + 10:card_y + 50, card_x:card_x + card_w]
+    row1_text = extract_text(row1_roi)
+
+    # Detect the 8 columns in row 2
+    row2_roi = masked_frame[card_y + 70:card_y + 100, card_x:card_x + card_w]
+    row2_gray = cv2.cvtColor(row2_roi, cv2.COLOR_BGR2GRAY)
+    _, row2_thresh = cv2.threshold(row2_gray, 100, 255, cv2.THRESH_BINARY_INV)
+    row2_contours, _ = cv2.findContours(row2_thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Filter out any contours that do not match the expected size and shape of the pink lines
+    pink_lines = []
+    for contour in row2_contours:  # Change row3_contours to row2_contours
+        x, y, w, h = cv2.boundingRect(contour)
+        aspect_ratio = w / h
+        if 0.5 < aspect_ratio < 2 and w > card_w * 0.05 and h > card_h * 0.04:
+            pink_lines.append(contour)
+
+    # Detect the white background in each column in row 3
+    row3_thresh = row2_thresh  # Use row2_thresh instead of row3_thresh since row3_thresh is not defined
+    row3_bg = cv2.bitwise_not(row3_thresh)
+    row3_bg_contours, _ = cv2.findContours(row3_bg, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Match the pink lines with their corresponding white background using the bounding boxes
+    rows = []
+    for i, pink_line in enumerate(pink_lines):
+        pink_x, pink_y, pink_w, pink_h = cv2.boundingRect(pink_line)
+        for white_bg in row3_bg_contours:
+            white_x, white_y, white_w, white_h = cv2.boundingRect(white_bg)
+            if abs(pink_x - white_x) < card_w * 0.05 and abs(pink_w - white_w) < card_w * 0.03:
+                rows.append((pink_line, white_bg))
+                break
+
+    # Extract the text from each column and row
+    results = {}
+    for i, (pink_line, white_bg) in enumerate(rows):
+        pink_x, pink_y, pink_w, pink_h = cv2.boundingRect(pink_line)
+        cells = []
+        cell_width = pink_w // cols
+        cell_height = pink_h
+        for j in range(cols):
+            cell = image[pink_y:pink_y + cell_height, pink_x + j * cell_width:pink_x + (j + 1) * cell_width]
+            cells.append(cell)
+        results[i] = cells
+
+    return cells
+    return row1_text, results, color_confidence
+
 def order_points(pts):
     rect = np.zeros((4, 2), dtype="float32")
 
@@ -15,6 +75,12 @@ def order_points(pts):
     rect[3] = pts[np.argmax(diff)]
 
     return rect
+
+def find_largest_contour_and_corners(contours):
+    largest_contour = max(contours, key=cv2.contourArea)
+    corners = cv2.approxPolyDP(largest_contour, 0.02 * cv2.arcLength(largest_contour, True), True)
+
+    return largest_contour, corners
 
 def four_point_transform(image, points):
     points = order_points(points)
@@ -43,44 +109,80 @@ def four_point_transform(image, points):
 
     return warped
 
+def preprocess_frame(frame):
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    _, thresh_frame = cv2.threshold(gray_frame, 127, 255, cv2.THRESH_BINARY_INV)
+    edged = cv2.Canny(thresh_frame, 50, 150)
+    _, edged = cv2.threshold(edged, 127, 255, cv2.THRESH_BINARY)
+
+    contours, _ = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    largest_contour = None
+    corners = None
+
+    if contours:
+        largest_contour, corners = find_largest_contour_and_corners(contours)
+
+    return largest_contour, corners
+
+def draw_results(image, row1_text, results, color_confidence):
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_scale = 0.8
+    thickness = 2
+
+    # Draw the row1_text on the image
+    cv2.putText(image, row1_text, (100, 50), font, font_scale, (0, 255, 0), thickness, cv2.LINE_AA)
+
+    # Draw the colored boxes and their text on the image
+    for i, (color, confidence) in enumerate(color_confidence.items()):
+        x, y, w, h = cv2.boundingRect(results[i])
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), thickness)
+        cv2.putText(image, f"{color}: {confidence:.2f}", (x, y - 10), font, font_scale, (0, 255, 0), thickness, cv2.LINE_AA)
+
+    return image
+
+def increase_contrast(frame, alpha=1.5, beta=30):
+    return cv2.addWeighted(frame, alpha, np.zeros(frame.shape, frame.dtype), 0, beta)
+
+    # Define the region of interest (ROI) that covers the drug test card
+    # You can adjust these values based on the size and position of the card in the camera's view
+    x1, y1, x2, y2 = 200, 200, 500, 600
+    roi = frame[y1:y2, x1:x2]
+
+    # Increase the contrast of the ROI
+    roi = increase_contrast(roi)
+
+    # Convert the ROI to grayscale
+    gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+
+    # Use adaptive thresholding to create a binary image
+    binary = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+
+    # Invert the binary image
+    inverted_binary = cv2.bitwise_not(binary)
+
+    # Apply edge detection to the inverted binary image
+    edged = cv2.Canny(inverted_binary, 50, 150)
+
+    # Find contours in the edged image
+    contours, _ = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # If a contour is found in the ROI, adjust its coordinates to the original frame
+    if contours:
+        contour = contours[0]
+        x, y, w, h = cv2.boundingRect(contour)
+        x += x1
+        y += y1
+        contour = np.array([[[x, y]], [[x + w, y]], [[x + w, y + h]], [[x, y + h]]], dtype=np.int32)
+
+    return contour
+
 def capture_image():
-    def increase_contrast(frame, alpha=1.2, beta=0):
-        return cv2.addWeighted(frame, alpha, np.zeros(frame.shape, frame.dtype), 0, beta)
-
-    def preprocess_frame(frame):
-        def increase_contrast(frame, alpha=1.5, beta=30):
-            return cv2.addWeighted(frame, alpha, np.zeros(frame.shape, frame.dtype), 0, beta)
-
-        frame = increase_contrast(frame)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-
-        # Use adaptive thresholding to create a binary image
-        binary = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-
-        # Invert the binary image
-        inverted_binary = cv2.bitwise_not(binary)
-
-        # Apply edge detection
-        edged = cv2.Canny(inverted_binary, 50, 150)
-        return edged
-
-    def find_largest_contour_and_corners(edged_frame):
-        contours, _ = cv2.findContours(edged_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        if contours:
-            largest_contour = max(contours, key=cv2.contourArea)
-            epsilon = 0.02 * cv2.arcLength(largest_contour, True)
-            corners = cv2.approxPolyDP(largest_contour, epsilon, True)
-            return largest_contour, corners
-        return None, None
-
     cap = cv2.VideoCapture(0)
 
     while True:
         ret, frame = cap.read()
         frame = cv2.flip(frame, 1)
-        edged_frame = preprocess_frame(frame)
-        largest_contour, corners = find_largest_contour_and_corners(edged_frame)
+        largest_contour, corners = preprocess_frame(frame)
 
         if largest_contour is not None and corners is not None and len(corners) == 4:
             # Draw the contour and corners on the frame
@@ -93,31 +195,13 @@ def capture_image():
 
         if cv2.waitKey(1) & 0xFF == ord(' '):
             if corners is not None and len(corners) == 4:
-                corners = np.array([corner[0] for corner in corners])
-                frame = four_point_transform(frame, corners)
-                cv2.imshow('Transformed Image', frame)  # Display the transformed image
-                cv2.waitKey(2000)  # Wait for 2 seconds (2000 ms)
-                cv2.destroyAllWindows()  # Close the window
-            break
+                corners = np.array([corner[0] for corner in corners], dtype="float32")
+                break
 
     cap.release()
     cv2.destroyAllWindows()
-    return frame
 
-def preprocess_image(image):
-    # Convert the image to grayscale if it has more than one channel
-    if len(image.shape) > 2 and image.shape[2] > 1:
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    else:
-        gray = image
-
-    # Apply binary thresholding
-    ret, thresholded = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-
-    # Invert the image
-    inverted = cv2.bitwise_not(thresholded)
-
-    return inverted
+    return frame, corners
 
 def get_dominant_color(image):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -256,28 +340,24 @@ def split_image_into_cells(image, rows, cols):
 
 
 def analyze_colored_boxes(image):
-    # Define the color range for red and blue in HSV color space
     lower_red = [0, 50, 50]
     upper_red = [10, 255, 255]
 
     lower_blue = [110, 50, 50]
     upper_blue = [130, 255, 255]
 
-    # Detect red areas in the image
     red_contours = detect_colored_areas(image, lower_red, upper_red)
-
-    # Detect blue areas in the image
     blue_contours = detect_colored_areas(image, lower_blue, upper_blue)
 
-    # Analyze the colors and store the confidence for each color
     red_confidence = analyze_colors(image, red_contours)
     blue_confidence = analyze_colors(image, blue_contours)
 
-    # Combine the color confidences
     color_confidence = {**red_confidence, **blue_confidence}
 
-    return color_confidence
+    # Combine the contours for red and blue
+    all_contours = red_contours + blue_contours
 
+    return color_confidence, all_contours
 
 def analyze_test_windows(control_windows, test_windows):
     results = []
@@ -305,58 +385,46 @@ def measure_line_intensity(line_img):
     return intensity
 
 def main():
-    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'  # Replace with the correct path for your system
-
     image = capture_image()
-    processed_image = process_image(image)
 
-    # Process the input image
-    skewed_image = correct_perspective(image)
+    # Preprocess the image
+    preprocessed_image = preprocess_image(image)
 
-    # Define the color range for red in HSV color space
-    lower_red = [0, 50, 50]
-    upper_red = [10, 255, 255]
+    # Extract the text from the image
+    text = extract_text(preprocessed_image)
+    print("Extracted text:", text)
 
-    lower_blue = [110, 50, 50]
-    upper_blue = [130, 255, 255]
-
-    # Detect red areas in the image
-    red_contours = detect_colored_areas(skewed_image, lower_red, upper_red)
-
-    # Detect blue areas in the image
-    blue_contours = detect_colored_areas(skewed_image, lower_blue, upper_blue)
-
-    # Split the image into cells
-    cells = split_image_into_cells(processed_image, 4, 2)
-
-    # Analyze the colored boxes
-    color_confidence = analyze_colored_boxes(skewed_image)
+    # Analyze the colors in the image
+    lower_color = [0, 100, 100]
+    upper_color = [10, 255, 255]
+    contours = detect_colored_areas(image, lower_color, upper_color)
+    color_confidence = analyze_colors(image, contours)
     print("Color confidence:", color_confidence)
 
-    # Assuming the third row contains the test windows
-    control_cells = [cells[2][i] for i in range(0, len(cells[2]), 2)]  # Even-numbered columns
-    test_cells = [cells[2][i] for i in range(1, len(cells[2]), 2)]  # Odd-numbered columns
+    # Split the image into cells
+    rows = 3
+    cols = 8
+    cells = split_image_into_cells(preprocessed_image, rows, cols)
 
-    test_windows = [{'control': control, 'test': test} for control, test in zip(control_cells, test_cells)]
-
-    # Analyze the test windows
-    test_windows = cells[2]  # Assuming the third row contains the test windows
-    test_results = analyze_test_windows(control_cells, test_cells)
-
-    # Extract text from each cell and store it in a nested list
-    table_data_text = []
+    # Process the cells
     for i, row in enumerate(cells):
-        table_row = []
         for j, cell in enumerate(row):
-            print(f"Processing cell ({i}, {j})")  # Add this print statement
-            text = extract_text(cell)
-            table_row.append(text)
-        table_data_text.append(table_row)
+            print(f"Processing cell ({i}, {j})")
+            cell_result = process_frame(cell)
+            print("Cell result:", cell_result)
 
-    # Create a pandas DataFrame from the nested list
-    table = pd.DataFrame(table_data_text)  # Use the renamed variable here
-
-    print(table)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
+    # Capture and process the image
+    captured_image, corners = capture_image()
+    corrected_image = four_point_transform(captured_image, corners)
+    row1_text, results, color_confidence = process_frame(corrected_image)
+
+    # Draw the results on the image and save it
+    output_image = draw_results(corrected_image, row1_text, results, color_confidence)
+    cv2.imwrite("output.png", output_image)
+
+    # Display the output image
+    cv2.imshow("Output", output_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
